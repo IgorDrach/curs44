ba371bdaaed18f2f6186fb5399196ed7d25f4962
ig.drach@gmail.com <ig.drach@gmail.com>
2024-12-17 11:13:20 +0300

    Добавьте файлы проекта.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Админ\Documents\Мои проекты\Институт\web designer\DreamTourRelaxAllYear\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
